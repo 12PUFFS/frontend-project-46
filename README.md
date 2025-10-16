59097b852f42c2ebc91f84c9d627a22f0920d30d
12 PUFFS <evgenypech98@gmail.com>
2025-10-16 20:57:16 +0300

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/12PUFFS/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/12PUFFS/frontend-project-46/actions)
+
+
+
+
+# Gendiff
+
+A CLI tool for comparing two configuration files and showing a difference.
+
+## Installation
+
+
+npm install
+npm link
+
+
+## Usage
+gendiff file1.json file2.json
+
+##Example
+https://asciinema.org/a/o8eZQVOHGcsURnVfyfDwMB5gH
--- src/gendiff.js
+++ src/gendiff.js
@@ -1,12 +1,32 @@
 import { parseFile } from './parsers.js';
+import _ from 'lodash';
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
   
-  // Временная реализация - просто выводим данные
-  // На следующих шагах здесь будет настоящая логика сравнения
-  return `File 1: ${JSON.stringify(data1, null, 2)}\nFile 2: ${JSON.stringify(data2, null, 2)}\nFormat: ${format}`;
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  
+  const diffLines = keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+    
+    if (!_.has(data1, key)) {
+      return `  + ${key}: ${value2}`;
+    }
+    
+    if (!_.has(data2, key)) {
+      return `  - ${key}: ${value1}`;
+    }
+    
+    if (_.isEqual(value1, value2)) {
+      return `    ${key}: ${value1}`;
+    }
+    
+    return `  - ${key}: ${value1}\n  + ${key}: ${value2}`;
+  });
+  
+  return `{\n${diffLines.join('\n')}\n}`;
 };
 
 export default genDiff;
