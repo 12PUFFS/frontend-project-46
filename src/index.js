660825ced87a907f8dda8573cdb90982f49d373a
12 PUFFS <evgenypech98@gmail.com>
2025-10-17 11:21:50 +0300

    step 4

--- src/index.js
+++ src/index.js
@@ -1,67 +1,117 @@
-const { readFileSync } = require('fs')
-const { extname } = require('path')
-const _ = require('lodash')
-const parse = require('./parsers')
-const formatStylish = require('./formatters/stylish')
+const { readFileSync } = require('fs');
+const { extname } = require('path');
+const _ = require('lodash');
+const parse = require('./parsers');
 
 const buildTree = (data1, data2) => {
-  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)))
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   return keys.map((key) => {
-    const value1 = data1[key]
-    const value2 = data2[key]
+    const value1 = data1[key];
+    const value2 = data2[key];
 
     if (!_.has(data1, key)) {
       return {
         key,
         value: value2,
-        type: 'added',
-      }
+        type: 'added'
+      };
     }
 
     if (!_.has(data2, key)) {
       return {
         key,
         value: value1,
-        type: 'removed',
-      }
+        type: 'removed'
+      };
+    }
+
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return {
+        key,
+        type: 'nested',
+        children: buildTree(value1, value2)
+      };
     }
 
     if (_.isEqual(value1, value2)) {
       return {
         key,
         value: value1,
-        type: 'unchanged',
-      }
+        type: 'unchanged'
+      };
     }
 
     return {
       key,
       oldValue: value1,
       value: value2,
-      type: 'updated',
+      type: 'updated'
+    };
+  });
+};
+
+const formatValue = (value, depth = 0) => {
+  if (!_.isObject(value) || _.isArray(value)) {
+    if (value === null) return 'null';
+    if (value === '') return "''";
+    return value;
+  }
+
+  const indent = '    '.repeat(depth);
+  const bracketIndent = '    '.repeat(depth - 1);
+  const entries = Object.entries(value);
+  const lines = entries.map(([key, val]) => {
+    const formattedValue = formatValue(val, depth + 1);
+    return `${indent}${key}: ${formattedValue}`;
+  });
+
+  return `{\n${lines.join('\n')}\n${bracketIndent}}`;
+};
+
+const formatStylish = (tree, depth = 1) => {
+  const indent = '    '.repeat(depth - 1);
+  const lines = tree.map((node) => {
+    const { key, type } = node;
+    const currentIndent = '    '.repeat(depth);
+
+    switch (type) {
+      case 'added':
+        return `${currentIndent.slice(0, -2)}+ ${key}: ${formatValue(node.value, depth + 1)}`;
+      case 'removed':
+        return `${currentIndent.slice(0, -2)}- ${key}: ${formatValue(node.value, depth + 1)}`;
+      case 'updated':
+        return `${currentIndent.slice(0, -2)}- ${key}: ${formatValue(node.oldValue, depth + 1)}\n${currentIndent.slice(0, -2)}+ ${key}: ${formatValue(node.value, depth + 1)}`;
+      case 'unchanged':
+        return `${currentIndent}${key}: ${formatValue(node.value, depth + 1)}`;
+      case 'nested':
+        return `${currentIndent}${key}: ${formatStylish(node.children, depth + 1)}`;
+      default:
+        throw new Error(`Unknown node type: ${type}`);
     }
-  })
-}
+  });
+
+  return `{\n${lines.join('\n')}\n${indent}}`;
+};
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
-  const content1 = readFileSync(filepath1, 'utf-8')
-  const content2 = readFileSync(filepath2, 'utf-8')
+  const content1 = readFileSync(filepath1, 'utf-8');
+  const content2 = readFileSync(filepath2, 'utf-8');
 
-  const format1 = extname(filepath1).slice(1)
-  const format2 = extname(filepath2).slice(1)
+  const format1 = extname(filepath1).slice(1);
+  const format2 = extname(filepath2).slice(1);
 
-  const data1 = parse(content1, format1)
-  const data2 = parse(content2, format2)
+  const data1 = parse(content1, format1);
+  const data2 = parse(content2, format2);
 
-  const tree = buildTree(data1, data2)
+  const tree = buildTree(data1, data2);
 
   switch (formatName) {
-  case 'stylish':
-    return formatStylish(tree)
-  default:
-    throw new Error(`Unsupported format: ${formatName}`)
+    case 'stylish':
+      return formatStylish(tree);
+    default:
+      throw new Error(`Unsupported format: ${formatName}`);
   }
-}
+};
 
-module.exports = genDiff
+module.exports = genDiff;
