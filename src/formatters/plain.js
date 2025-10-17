5c5e79866ab6a7d5ea9468c9717fc6e8a3ba87b8
12 PUFFS <evgenypech98@gmail.com>
2025-10-17 12:02:50 +0300

    step 4

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,42 +1,37 @@
 const _ = require('lodash')
 
-const formatPlain = (diff, path = '') => {
-  const lines = diff.flatMap((node) => {
-    const currentPath = path ? `${path}.${node.key}` : node.key
+const formatValueForPlain = (value) => {
+  if (_.isObject(value) && !_.isArray(value)) {
+    return '[complex value]'
+  }
+  if (typeof value === 'string') {
+    return `'${value}'`
+  }
+  return value
+}
 
-    switch (node.type) {
+const formatPlain = (tree, path = '') => {
+  const lines = tree.flatMap((node) => {
+    const { key, type } = node
+    const currentPath = path ? `${path}.${key}` : key
+
+    switch (type) {
     case 'added':
-      return `Property '${currentPath}' was added with value: ${formatValue(node.value)}`
+      return `Property '${currentPath}' was added with value: ${formatValueForPlain(node.value)}`
     case 'removed':
       return `Property '${currentPath}' was removed`
-    case 'changed':
-      return `Property '${currentPath}' was updated. From ${formatValue(node.oldValue)} to ${formatValue(node.newValue)}`
+    case 'updated':
+      return `Property '${currentPath}' was updated. From ${formatValueForPlain(node.oldValue)} to ${formatValueForPlain(node.value)}`
     case 'nested':
       return formatPlain(node.children, currentPath)
     case 'unchanged':
       return []
     default:
-      return []
+      throw new Error(`Unknown node type: ${type}`)
     }
   })
 
   return lines.join('\n')
-}
-
-const formatValue = (value) => {
-  if (_.isPlainObject(value)) {
-    return '[complex value]'
-  }
-
-  if (typeof value === 'string') {
-    return `'${value}'`
-  }
-
-  if (value === null) {
-    return 'null'
-  }
-
-  return String(value)
 }
 
 module.exports = formatPlain
