52b11a3255c1d5d8edb059f46c9bb115011d972b
12PUffS <evgenypech98@gmail.com>
2026-03-14 20:26:49 +0300

    fix: remove lint from Makefile

--- src/parsers.js
+++ src/parsers.js
@@ -2,7 +2,7 @@ import fs from 'node:fs';
 import path from 'node:path';
 import yaml from 'js-yaml';
 
-export default function parseFile (filepath) {
+export default function parseFile(filepath) {
   const content = fs.readFileSync(filepath, 'utf-8');
   const ext = path.extname(filepath);
 
