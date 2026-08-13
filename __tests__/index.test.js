afded8149cb0e2755057819f3ce5153a57eda11f
12PUffS <evgenypech98@gmail.com>
2026-08-13 16:02:11 +0300

    10 step done

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -143,6 +143,172 @@ Property 'group3' was added with value: [complex value]`;
     });
   });
 
+  describe('json format', () => {
+    it('should compare flat json files', () => {
+      const file1 = getFixturePath('file1.json');
+      const file2 = getFixturePath('file2.json');
+
+      const result = genDiff(file1, file2, 'json');
+
+      const expected = `[
+  {
+    "key": "follow",
+    "type": "removed",
+    "value": false
+  },
+  {
+    "key": "host",
+    "type": "unchanged",
+    "value": "hexlet.io"
+  },
+  {
+    "key": "proxy",
+    "type": "removed",
+    "value": "123.234.53.22"
+  },
+  {
+    "key": "timeout",
+    "type": "changed",
+    "oldValue": 50,
+    "newValue": 20
+  },
+  {
+    "key": "verbose",
+    "type": "added",
+    "value": true
+  }
+]`;
+
+      expect(result).toBe(expected);
+    });
+
+    it('should compare nested json files', () => {
+      const file1 = getFixturePath('file1_nested.json');
+      const file2 = getFixturePath('file2_nested.json');
+
+      const result = genDiff(file1, file2, 'json');
+
+      const expected = `[
+  {
+    "key": "common",
+    "type": "nested",
+    "children": [
+      {
+        "key": "follow",
+        "type": "added",
+        "value": false
+      },
+      {
+        "key": "setting1",
+        "type": "unchanged",
+        "value": "Value 1"
+      },
+      {
+        "key": "setting2",
+        "type": "removed",
+        "value": 200
+      },
+      {
+        "key": "setting3",
+        "type": "changed",
+        "oldValue": true,
+        "newValue": null
+      },
+      {
+        "key": "setting4",
+        "type": "added",
+        "value": "blah blah"
+      },
+      {
+        "key": "setting5",
+        "type": "added",
+        "value": {
+          "key5": "value5"
+        }
+      },
+      {
+        "key": "setting6",
+        "type": "nested",
+        "children": [
+          {
+            "key": "doge",
+            "type": "nested",
+            "children": [
+              {
+                "key": "wow",
+                "type": "changed",
+                "oldValue": "",
+                "newValue": "so much"
+              }
+            ]
+          },
+          {
+            "key": "key",
+            "type": "unchanged",
+            "value": "value"
+          },
+          {
+            "key": "ops",
+            "type": "added",
+            "value": "vops"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "key": "group1",
+    "type": "nested",
+    "children": [
+      {
+        "key": "baz",
+        "type": "changed",
+        "oldValue": "bas",
+        "newValue": "bars"
+      },
+      {
+        "key": "foo",
+        "type": "unchanged",
+        "value": "bar"
+      },
+      {
+        "key": "nest",
+        "type": "changed",
+        "oldValue": {
+          "key": "value"
+        },
+        "newValue": "str"
+      }
+    ]
+  },
+  {
+    "key": "group2",
+    "type": "removed",
+    "value": {
+      "abc": 12345,
+      "deep": {
+        "id": 45
+      }
+    }
+  },
+  {
+    "key": "group3",
+    "type": "added",
+    "value": {
+      "deep": {
+        "id": {
+          "number": 45
+        }
+      },
+      "fee": 100500
+    }
+  }
+]`;
+
+      expect(result).toBe(expected);
+    });
+  });
+
   it('should handle empty objects', () => {
     const emptyFile = getFixturePath('empty.json');
 
